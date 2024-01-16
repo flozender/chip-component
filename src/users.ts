681a1bae3dddbb129780d7c1213542e231007fed
Tayeeb Hasan <tayeeb.hasan@gmail.com>
2024-01-16 01:48:45 -0500

    set up faker.js for user generation

---
+++ src/users.ts
@@ -0,0 +1,17 @@
+import { faker } from "@faker-js/faker";
+
+export type User = {
+  name: string;
+  email: string;
+};
+
+export function createRandomUser(): User {
+  return {
+    name: faker.person.fullName(),
+    email: faker.internet.email(),
+  };
+}
+
+export const USERS: User[] = faker.helpers.multiple(createRandomUser, {
+  count: 10,
+});
